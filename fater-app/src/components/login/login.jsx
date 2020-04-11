788eef4a77a024f3e88f24c72e00476929bfdb53
Hamed <h4lmed@gmail.com>
2020-04-11 11:06:37 +0430

    stable area

--- fater-app/src/components/login/login.jsx
+++ fater-app/src/components/login/login.jsx
@@ -1,8 +1,53 @@
 import React, { Component } from "react";
 import "./login.css";
 import login from "./login.png";
+import $ from "jquery";
+import { IonIcon } from "@ionic/react";
+import { eye, eyeOff } from "ionicons/icons";
 
 class Login extends Component {
+  onSignIn() {
+    console.log("signed");
+  }
+
+  onPassChange() {
+    const passValue = $("#password").val();
+    if (passValue.length > 0) {
+      if ($("#password")[0].type === "password") {
+        $("#password-eye").removeClass("hide");
+        $("#password-eye").addClass("active");
+      }
+    } else {
+      $("#password-eye").addClass("hide");
+      $("#password-eye").removeClass("active");
+    }
+  }
+
+  togglePassEye() {
+    console.log($("#password").type);
+    const isPasswordShown = $("#password-eyeoff").hasClass("active");
+
+    if (isPasswordShown) {
+      $("#password-eyeoff").removeClass("active");
+      $("#password-eyeoff").addClass("hide");
+
+      $("#password-eye").addClass("active");
+      $("#password-eye").removeClass("hide");
+
+      $("#password")[0].type = "password";
+    } else {
+      $("#password-eye").removeClass("active");
+      $("#password-eye").addClass("hide");
+
+      $("#password-eyeoff").addClass("active");
+      $("#password-eyeoff").removeClass("hide");
+
+      $("#password")[0].type = "text";
+    }
+  }
+
+  componentDidMount() {}
+
   render() {
     return (
       <div className="container">
@@ -19,27 +64,39 @@ class Login extends Component {
             <span id="reauth-email" className="reauth-email"></span>
             <input
               type="email"
-              id="inputEmail"
+              id="email"
               className="form-control"
               placeholder="Email address"
               required
               autoFocus
             />
-            <input
-              type="password"
-              id="inputPassword"
-              className="form-control"
-              placeholder="Password"
-              required
-            />
-            <div id="remember" className="checkbox">
-              <label>
-                <input type="checkbox" value="remember-me" /> Remember me
-              </label>
+            <div id="passwordContainer">
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                placeholder="Password"
+                required
+                onChange={this.onPassChange}
+              />
+              <span onClick={this.togglePassEye}>
+                <IonIcon
+                  className="passwordEye hide"
+                  id="password-eyeoff"
+                  icon={eyeOff}
+                />
+                <IonIcon
+                  className="passwordEye hide"
+                  id="password-eye"
+                  icon={eye}
+                />
+              </span>
             </div>
+
             <button
               className="btn btn-lg btn-primary btn-block btn-signin"
               type="submit"
+              onSubmit={this.onSignIn}
             >
               Sign in
             </button>
